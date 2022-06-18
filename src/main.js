6f4557cb24237845a7c69d9b37dca97ef9d2902a
Elimar Rojas <ej.rojasordaz@gmail.com>
2022-06-17 19:00:09 -0500

    Creación de lista de películas en tendencia

---
+++ src/main.js
@@ -0,0 +1,23 @@
+async function getTrendingMovies() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log({data, movies});
+
+    movies.forEach(movie => {
+        const trendMovieContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+        
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+        
+        movieContainer.appendChild(movieImg);
+        trendMovieContainer.appendChild(movieContainer);
+    });
+}
+getTrendingMovies();
